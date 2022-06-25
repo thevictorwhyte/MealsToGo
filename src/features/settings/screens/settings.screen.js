2ef84cc27d300dd4a4286a38df9d8c33721930a2
thevictorwhyte <victordavidwhyte@gmail.com>
2022-06-25 20:55:41 +0100

    feat: set up camera screen infastructure

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { TouchableOpacity } from 'react-native'
 import styled from "styled-components/native";
 
 import { List, Avatar } from "react-native-paper";
@@ -22,7 +23,9 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
