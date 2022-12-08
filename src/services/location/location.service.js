87e411e48e57e40e24bcaf7ad465a0126f8448bf
Victor Whyte <victorwhyte@macs-MacBook-Pro.local>
2022-12-08 14:44:11 +0100

    fix: parse depending on whether using mock or not

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -2,11 +2,18 @@ import camelize from "camelize";
 import { host, isMock } from "../../utils/env";
 
 export const locationRequest = (searchTerm) => {
-  return fetch(`${host}/geocode?city=${searchTerm}&mock=${isMock}`)
-    .then((res) => {
+  if (isMock) {
+    return fetch(`${host}/geocode?city=${searchTerm}&mock=${isMock}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => JSON.parse(data));
+  }
+  return fetch(`${host}/geocode?city=${searchTerm}&mock=${isMock}`).then(
+    (res) => {
       return res.json();
-    })
-    .then((data) => JSON.parse(data));
+    }
+  );
 };
 
 export const locationTransform = (result) => {
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,8 @@
 import { Platform } from "react-native";
 
-const liveHost = "https://us-central1-meals-to-go-749bd.cloudfunctions.net";
-const localHost = "http://127.0.0.1:5001/meals-to-go-749bd/us-central1";
+export const liveHost =
+  "https://us-central1-meals-to-go-749bd.cloudfunctions.net";
+export const localHost = "http://127.0.0.1:5001/meals-to-go-749bd/us-central1";
 export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const host = !isDevelopment || isAndroid ? liveHost : localHost;
