7bebc618825c086cbc420c864fcf50d685a5d55f
Victor Whyte <victorwhyte@macs-MacBook-Pro.local>
2022-12-08 13:13:34 +0100

    feat: switch between mock modes

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,3 +2,4 @@ const liveHost = "https://us-central1-meals-to-go-749bd.cloudfunctions.net";
 const localHost = "http://127.0.0.1:5001/meals-to-go-749bd/us-central1";
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const host = isDevelopment ? localHost : liveHost;
+export const isMock = false; 
