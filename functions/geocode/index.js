a22d0fa399eecda7b5fbbf2192d78e897d0eb9e6
Victor Whyte <victorwhyte@macs-MacBook-Pro.local>
2022-12-08 12:02:00 +0100

    feat: integrate google geocode with cloud functions

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,26 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
-
-  response.json(JSON.stringify(locationMock));
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(JSON.stringify(locationMock));
+  }
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((error) => {
+      response.status(400);
+      return response.send(error.response.data.error_message);
+    });
 };
